c67e2cc03c8a581caf810f4bfe0baa8bd1a91f31
Shashank Singh <95364209+ShashankSin@users.noreply.github.com>
2024-03-03 16:03:04 +0545

    first commit

--- script/script.js
+++ script/script.js
@@ -1,20 +1,20 @@
 function residental() {
   document.getElementsByClassName(
     "card--container__image"
-  ).style.backgroundImage = "url(/images)";
+  ).style.backgroundImage = "url('images/card-1.jpg')";
 }
 function commercial() {
   document.getElementsByClassName(
     "card--container__image"
-  ).style.backgroundImage = "url(/images/card-2.jpg)";
+  ).style.backgroundImage = "url('images/card-2.jpg')";
 }
 function industrial() {
   document.getElementsByClassName(
     "card--container__image"
-  ).style.backgroundImage = "url(/images/card-3.png)";
+  ).style.backgroundImage = "url('images/card-3.png')";
 }
 function pool() {
   document.getElementsByClassName(
     "card--container__image"
-  ).style.backgroundImage = "url(/images/card-4.png)";
+  ).style.backgroundImage = "url('images/card-4.png')";
 }
