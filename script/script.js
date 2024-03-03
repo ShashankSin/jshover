e1c7ec6e1dcb89bee251b34019c9d42bc5cfcaf5
Shashank Singh <95364209+ShashankSin@users.noreply.github.com>
2024-03-03 15:51:18 +0545

    first commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function residental() {
+  document.getElementsByClassName(
+    "card--container__image"
+  ).style.backgroundimage = "url(/images/card-1.jpg)";
+}
+function commercial() {}
+function industrial() {}
+function pool() {}
